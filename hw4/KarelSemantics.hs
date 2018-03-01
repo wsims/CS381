f306cb3f4439d9f0f203166fc0421c41332efdd2
wsims <willy.simz@gmail.com>
2018-02-28 18:57:58 -0800

    finishes part 5 and 6

--- hw4/KarelSemantics.hs
+++ hw4/KarelSemantics.hs
@@ -1,3 +1,4 @@
+-- Authors: Dan Lin (lintzu), Will Sims (simsw), Cameron Friel (frielc)
 module KarelSemantics where
 
 import Prelude hiding (Either(..))
@@ -21,16 +22,16 @@ stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown   _ _ r = Done r
 stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
+                            then OK (decBeeper p w) (incBag r)
+                            else Error ("No beeper to pick at: " ++ show p)
 stmt PutBeeper _ w r = let p = getPos r
                        in if isEmpty r
-                            then Error ("No beeper. Well too bad, so sad")
+                            then Error ("No beeper to put.")
                             else OK (incBeeper p w) (decBag r)
 stmt Move _ w (p, c, b) = let np = neighbor c p
-                  in if isClear np w
-                        then OK w (setPos np (p, c, b))
-                        else Error ("Blocked at : " ++ show p)
+                          in if isClear np w
+                                then OK w (setPos np (p, c, b))
+                                else Error ("Blocked at: " ++ show np)
 stmt (Turn d) _ w r = OK w (setFacing (cardTurn d (getFacing r)) r)
 stmt (Block []) _ w r = OK w r
 stmt (Block (x:xs)) d w r = case stmt x d w r of
@@ -38,8 +39,20 @@ stmt (Block (x:xs)) d w r = case stmt x d w r of
                             Done r' -> Done r'
                             Error m -> Error m
 stmt (If t s1 s2) d w r = case test t w r of
-                              t' -> if t' then stmt s1 d w r else stmt s2 d w r
-
+                        t' -> if t' then stmt s1 d w r else stmt s2 d w r
+stmt (Call m) d w r = case lookup m d of
+                            (Just b) -> stmt b d w r
+                            _        -> Error ("Undefined macro: " ++ m)
+stmt (Iterate i b)  d w r = if i > 0 then case stmt b d w r of
+                                OK w' r' -> stmt (Iterate (i-1) b) d w' r'
+                                Done r' -> Done r'
+                                Error m -> Error m
+                            else OK w r
+stmt (While t b) d w r = if test t w r then case stmt b d w r of
+                                OK w' r' -> stmt (While t b) d w' r'
+                                Done r' -> Done r'
+                                Error m -> Error m
+                            else OK w r
 
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
